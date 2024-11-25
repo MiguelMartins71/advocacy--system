a9d59cb6f6477dcc2c2e225e15f8a5cce23ae2d1
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2024-11-25 15:25:47 -0300

    REPO - User

---
+++ Backend/Lexfile/Lexfile/src/main/java/com/example/Lexfile/repositories/CasoRepository.java
@@ -0,0 +1,12 @@
+package com.example.Lexfile.repositories;
+
+import com.example.Lexfile.models.Caso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CasoRepository extends JpaRepository<Caso, Long> {
+
+
+
+
+    
+}
---
+++ Backend/Lexfile/Lexfile/src/main/java/com/example/Lexfile/repositories/PerfilRepository.java
@@ -0,0 +1,6 @@
+package com.example.Lexfile.repositories;
+
+import com.example.Lexfile.models.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfilRepository extends JpaRepository<Perfil, Long> {}
